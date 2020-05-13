ae9591d2610207afeaf7da2030f001f7273ae193
justweihong <justweihong@gmail.com>
2020-05-13 20:31:51 +0800

    Revert "testing 1 line"
    This reverts commit efeef58fea3cdeaa5f69223fd267c1fc9a99a27d.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'explore', component: ExploreComponent},
     //{ path: 'explore', redirectTo: '/explore'},
-    //testing
 ];
 
 @NgModule({
