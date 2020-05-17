98caadf7a34ca72c7191c0df66b2a2cbad6b3bb4
CodyChew <chewchengyap@gmail.com>
2020-05-17 13:03:41 +0800

    Updated explore page with requests page
    Used mattabs from angular material for tabs component in request page
    
    -included MatTabsModule & BrowserAnimationsModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { ExploreComponent } from './pages/explore/explore.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RequestPageComponent } from './pages/request-page/request-page.component';
 
 
 const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent},
-    { path: 'explore', component: ExploreComponent},
+    { path: 'explore', component: ExploreComponent,
+        children: [{path: 'request-page', component: RequestPageComponent}]}
+    
     {path: '**', redirectTo: 'home'}
     //{ path: 'explore', redirectTo: '/explore'},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 import * as firebase from "firebase/app";
 import { LoginComponent } from './pages/login/login.component';
 import { ExploreComponent } from './pages/explore/explore.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RequestListingCardComponent } from './components/request/request-listing-card/request-listing-card.component';
-import { HomeComponent } from './pages/home/home.component'
-
+import { HomeComponent } from './pages/home/home.component';
+import { RequestPageComponent } from './pages/request-page/request-page.component';
+import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -20,10 +24,13 @@ import { HomeComponent } from './pages/home/home.component'
     NavbarComponent,
     RequestListingCardComponent,
     HomeComponent,
+    RequestPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
