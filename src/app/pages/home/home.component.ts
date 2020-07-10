2d718378f258be0eeabd7235677ddc554e76d716
justweihong <justweihong@gmail.com>
2020-07-11 00:02:58 +0800

    more changes on indiv feature section, scrolling clikc still dosent work

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router } from '@angular/router';
+// declare var $ : any;
+import * as $ from 'jquery';
 
 
 @Component({
@@ -17,14 +19,31 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     $(document).ready(function() {
-      $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-      });
+      // $('#sidebarCollapse').on('click', function () {
+      //     $('#sidebar').toggleClass('active');
+      // });
+
+      $("#request-feature").click(function (){
+        console.log("hello")
+        $('html, body').animate({
+            scrollTop: $("#chat-feature").offset().top
+        }, 2000);
+        // $('html, body').animate({ scrollTop: 0 }, 600);
+    });
+
   })
   }
 
   playVid() {
     var vid = document.getElementById("walkthrough-video");
 
+  }
+
+  scroll2(){
+    console.log("hell2")
+    var target = $(`#request-section2`)
+    $('html, body').animate({
+      scrollTop: target.offset().top - 50
+    }, 1000);
   }
 }
